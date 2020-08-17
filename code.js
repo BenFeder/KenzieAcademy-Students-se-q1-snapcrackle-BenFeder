80e7cffbb25c75b8c47f912294e1ab599f375e8a
BenFeder <BenjaminTFeder@gmail.com>
2020-08-17 12:49:41 -0400

    First add of snapCrackle + Prime

--- code.js
+++ code.js
@@ -1,2 +1,35 @@
 // Create your function here
 // Log your output to the console.
+function snapCrackle(maxValue) {
+  let string1 = "";
+  for (i = 1; i <= maxValue; i++) {
+    if (i % 2 != 0) {
+      string1 += "Snap, ";
+    } else if (i % 5 == 0) {
+      string1 += "Crackle, ";
+    } else if (i % 2 != 0 && i % 5 == 0) {
+      string1 += "SnapCrackle, ";
+    } else {
+      string1 += i + ", ";
+    }
+  }
+  console.log(string1);
+}
+
+function snapCracklePrime(maxValue) {
+  let string1 = "";
+  for (i = 1; i <= maxValue; i++) {
+    if (i % 2 != 0) {
+      string1 += "Snap, ";
+    } else if (i % 5 == 0) {
+      string1 += "Crackle, ";
+    } else if (i % 2 != 0 && i % 5 == 0) {
+      string1 += "SnapCrackle, ";
+    } else if (i > 1 && i % 2 != 0 && i % 3 != 0 && i % 5 != 0 && i % 7 != 0) {
+      string += "Prime, ";
+    } else {
+      string1 += i + ", ";
+    }
+  }
+  console.log(string1);
+}
