afcd0b1f17e279ced01999d760d97836a2bd34d3
BenFeder <BenjaminTFeder@gmail.com>
2020-08-17 13:26:12 -0400

    Second add of snapCrackle + Prime, updated and fixed

--- code.js
+++ code.js
@@ -3,12 +3,12 @@
 function snapCrackle(maxValue) {
   let string1 = "";
   for (i = 1; i <= maxValue; i++) {
-    if (i % 2 != 0) {
+    if (i % 2 != 0 && i % 5 == 0) {
+      string1 += "SnapCrackle, ";
+    } else if (i % 2 != 0) {
       string1 += "Snap, ";
     } else if (i % 5 == 0) {
       string1 += "Crackle, ";
-    } else if (i % 2 != 0 && i % 5 == 0) {
-      string1 += "SnapCrackle, ";
     } else {
       string1 += i + ", ";
     }
@@ -16,20 +16,30 @@ function snapCrackle(maxValue) {
   console.log(string1);
 }
 
+// Bonus snapCracklePrime function
 function snapCracklePrime(maxValue) {
   let string1 = "";
   for (i = 1; i <= maxValue; i++) {
-    if (i % 2 != 0) {
+    if (
+      i > 1 &&
+      (i % 2 != 0 || i == 2) &&
+      (i % 3 != 0 || i == 3) &&
+      (i % 5 != 0 || i == 5) &&
+      (i % 7 != 0 || i == 7)
+    ) {
+      string1 += "Prime, ";
+    } else if (i % 2 != 0 && i % 5 == 0) {
+      string1 += "SnapCrackle, ";
+    } else if (i % 2 != 0) {
       string1 += "Snap, ";
     } else if (i % 5 == 0) {
       string1 += "Crackle, ";
-    } else if (i % 2 != 0 && i % 5 == 0) {
-      string1 += "SnapCrackle, ";
-    } else if (i > 1 && i % 2 != 0 && i % 3 != 0 && i % 5 != 0 && i % 7 != 0) {
-      string += "Prime, ";
     } else {
       string1 += i + ", ";
     }
   }
   console.log(string1);
 }
+// Tests with maxValue = 100
+snapCrackle(100);
+snapCracklePrime(100);
